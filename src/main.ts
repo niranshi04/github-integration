8c2c8074f14f42f277c988f26930bc7ce0e93b10
niranshi04 <niranshi.agarwal.cse19@itbhu.ac.in>
2022-08-18 21:04:42 +0530

    Adding support for variable ports for heroku

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,6 @@ async function bootstrap() {
   app.use(passport.initialize());
   app.use(passport.session());
 
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
